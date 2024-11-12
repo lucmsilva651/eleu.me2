09f7a86a468fa1f802fa63b72e2ee0eccd1e4e2b
Lucas Gabriel <lucmsilva651@gmail.com>
2024-11-12 20:45:27 -0300

    Push Materialize website

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+// serve public files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on localhost:${PORT}`);
+});
