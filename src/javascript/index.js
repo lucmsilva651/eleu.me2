162394083c45a033a71ccc67d25c6e2bfdf9ecf4
Luquinhas <lucmsilva651@gmail.com>
2025-01-06 10:17:32 -0300

    Vercel Speed Insights

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,7 +1,9 @@
 import { loadComponents } from './components.min.js';
 import { loadTyped } from './typed.min.js';
 import { loadZeroMd } from './zeromd.min.js';
+import { injectSpeedInsights } from 'https://cdn.jsdelivr.net/npm/@vercel/speed-insights@latest';
 
+injectSpeedInsights();
 loadZeroMd();
 loadComponents();
 loadTyped();
