a7d7dc32d2dd201018fa22d0ec3124bf4fd280da
Luquinhas <lucmsilva651@gmail.com>
2025-01-06 10:23:44 -0300

    Revert "Vercel Speed Insights"
    This reverts commit 162394083c45a033a71ccc67d25c6e2bfdf9ecf4.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,9 +1,7 @@
 import { loadComponents } from './components.min.js';
 import { loadTyped } from './typed.min.js';
 import { loadZeroMd } from './zeromd.min.js';
-import { injectSpeedInsights } from 'https://cdn.jsdelivr.net/npm/@vercel/speed-insights@latest';
 
-injectSpeedInsights();
 loadZeroMd();
 loadComponents();
 loadTyped();
